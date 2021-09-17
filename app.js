6e3eebe3cac18288ee8029909b11cf9429adadaf
Joao Gabriel <joaogabrielferr@gmail.com>
2021-09-16 22:17:20 -0300

    mudando forma de formatacao da operacao

--- app.js
+++ app.js
@@ -1,159 +1,45 @@
-let telastr = "";
-let currstr = "";
-const arraystr = [];
+tela = "";
+atual = "";
 
-let calculado = false;
 
-//coloca os numeros na tela
-let numeros = document.getElementsByClassName("botao");
+let atualizatela = function()
+{
+    document.getElementById("inputID").value = tela;
+    cursor(document.getElementById("inputID"));
+}
+
+
+//pega numero e coloca na tela
 let addnumero = function()
 {
-    telastr+=this.innerHTML;
-    currstr+=this.innerHTML;
-    document.getElementById("inputID").value = telastr;
-    cursor(document.getElementById("inputID"));
-
+    atual+=this.innerHTML;
+    tela+=this.innerHTML;
+    atualizatela();
+    
 }
+let numeros = document.getElementsByClassName("numero");
 for(i = 0;i<numeros.length;i++)
 {
-    numeros[i].addEventListener("click",addnumero)
+    numeros[i].addEventListener("click",addnumero);
 }
 
-//apaga a conta atual clicando no botao AC
-let ac = document.getElementById("opAC");
-ac.addEventListener("click",()=>{
-    telastr = "";
-    document.getElementById("inputID").value = "0";
-    arraystr.length = 0;
-    document.getElementById("ans").innerHTML = "Ans";
-});
+//adiciona operação
 
-let ans = document.getElementById("ans").addEventListener("click",()=>{
-    if(currstr=="" && !calculado)
-    {
-        currstr = document.getElementById("ans").innerHTML;
-        telastr+=currstr;
-        calculado = false;
-    }
-});
-
-//todo: ta add o ! msm sem numero no inicio
-let opfac = document.getElementById("opfac").addEventListener("click",()=>{
-    if(currstr!=""){
-    currstr+="!";
-    telastr+="!";
-    arraystr.push(currstr);
-    document.getElementById("inputID").value = telastr;
-    }
-});
-
-
-let addop = function(){
-    if(currstr != ""){
-    telastr += " " + this.innerHTML + " ";
-    arraystr.push(currstr);
-    arraystr.push(this.innerHTML)
-    currstr = "";
-    document.getElementById("inputID").value = telastr;
-    cursor(document.getElementById("inputID"));  
-    }
-}
-
-let opbts = document.getElementsByClassName("op");
-for(i = 0;i<opbts.length;i++)
+let addop = function()
 {
-    opbts[i].addEventListener("click",addop);
+    if(atual[atual.length - 1] >= '0' && atual[atual.length - 1] <= '9')
+    {
+    atual+=this.innerHTML;
+    tela+= (" " + this.innerHTML + " ");
+    }
+    atualizatela();
 }
-
-//converte string para numero
-const converte = function(strnum)
+let ops = document.getElementsByClassName("op");
+for(i = 0;i<ops.length;i++)
 {
-    //for(const el of strnum)console.log(el);
-    let r = 0;
-    let pow = strnum.length - 1;
-    for(const el of strnum)
-    {
-        let n = el.charCodeAt(0) - 48;
-        r+= (n * (10**pow));
-        pow--;
-    }
-    return r;
+    ops[i].addEventListener("click",addop)
 }
 
-
-//calcula a equação
-let calcula = function(){
-
-    arraystr.push(currstr);
-    currstr = "";
-    let multi = 0,div = 0,soma = 0,sub = 0,fac = 0;
-    let resultado = 0;
-    for(const el of arraystr)
-    {
-        if(el == "x")multi++;
-        if(el == "÷")div++;
-        if(el == "+")soma++;
-        if(el == "-")sub++;
-        if(el[el.length - 1] == '!')fac++;
-    }
-
-    while(arraystr.length > 1){
-    
-    while(fac > 0)
-    {
-        for(i = 0;i<arraystr.length;i++)
-        {
-            let aux = arraystr[i];
-            if(aux[aux.length - 1] == '!')
-            {
-                let numstr = aux.replace("!","");
-                let num = parseInt(numstr,10);
-                let resultado = 1;
-                for(i = 2;i<=num;i++)
-                {
-                    resultado*=i;
-                }
-                let resultstring = resultado.toString();
-                arraystr[i] = resultstring;
-                fac--;
-                break;
-
-            }
-        }
-    }
-
-    while(multi > 0)
-    {
-        for(i = 0;i<arraystr.length;i++)
-        {
-            if(arraystr[i] == "x")
-            {
-                let n1 = converte(arraystr[i-1]);
-                let n2 = converte(arraystr[i+1]);
-                let resultado = n1*n2;
-                let resultstring = resultado.toString();
-                arraystr.splice(i+2,0,resultstring);
-                console.log(arraystr);
-                arraystr.splice(i-1,3);
-                multi--;                
-                break;
-            }
-        }
-    }
-    }
-    console.log(arraystr);
-    document.getElementById("ans").innerHTML = arraystr[0];
-    document.getElementById("inputID").value = arraystr[0];
-    currstr = arraystr[0];
-    telastr = currstr; 
-    arraystr.length = 0;
-    calculado = true; 
-}
-
-
-
-let btnR = document.getElementById("R");
-btnR.addEventListener("click",calcula);
 
 //https://www.geeksforgeeks.org/how-to-place-cursor-position-at-end-of-text-in-text-input-field-using-javascript/
 let cursor = function PosEnd(end) {
